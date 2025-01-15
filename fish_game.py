69dee617b244adae5acfd42173eb7d6a12bcc9fc
Fazıl Can <fzcelektro07@gmail.com>
2025-01-15 13:55:47 +0300

    oyun döngüsü oluşturuldu

--- fish_game.py
+++ fish_game.py
@@ -10,3 +10,13 @@ win = display.set_mode((genislik,yukseklik))
 fps = 60
 
 clock = time.Clock()
+
+durum = True
+while durum:
+    for i in event.get():
+       if i.type == QUIT:
+           durum = False
+
+    display.update()
+    clock.tick()
+quit()
