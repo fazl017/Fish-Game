4839ff1b65d3ea3a9217b6208a79dfa1a3814946
Fazıl Can <fzcelektro07@gmail.com>
2025-01-15 14:10:28 +0300

    sınıflar olusturuldu

--- fish_game.py
+++ fish_game.py
@@ -11,6 +11,58 @@ fps = 60
 
 clock = time.Clock()
 
+
+class Game():
+    def __init__(self):
+        pass
+
+    def update(self):
+        pass
+
+    def cizdir(self):
+        pass
+
+    def temas(self):
+        pass
+
+    def stop(self):
+        pass
+
+    def reset(self):
+        pass
+
+    def guvenli_alan(self):
+        pass
+
+    def hedef_yenile(self):
+        pass
+
+    def hedefle(self):
+        pass
+
+
+class Fish(sprite.Sprite):
+    def __init__(self):
+        pass
+
+    def update(self, ):
+        pass
+
+
+
+class Balikci (sprite.Sprite):
+    def __init__(self):
+        pass
+
+    def update(self,):
+        pass
+
+    def hareket(self):
+        pass
+
+
+
+
 durum = True
 while durum:
     for i in event.get():
